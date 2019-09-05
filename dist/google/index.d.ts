ace5b75b49eb59d389cf954bdff90a949bb2ce94
Miguel Caballero <mcabs3@gmail.com>
2019-09-05 16:03:11 -0500

    updated gitignore to exclude dist

--- dist/google/index.d.ts
+++
@@ -1,4 +0,0 @@
-import Places, { GooglePlaces } from "./Places";
-export { GOOGLE_PLACES_API, API } from "./Constants";
-export { GooglePlaces };
-export default Places;
--- dist/google/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Places_1 = require("./Places");
-exports.GooglePlaces = Places_1.GooglePlaces;
-var Constants_1 = require("./Constants");
-exports.GOOGLE_PLACES_API = Constants_1.GOOGLE_PLACES_API;
-exports.API = Constants_1.API;
-exports.default = Places_1.default;
-//# sourceMappingURL=index.js.map
